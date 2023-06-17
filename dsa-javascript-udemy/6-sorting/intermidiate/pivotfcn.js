8730ce2c03d3465dc6e671e0c7fa399239e0597c
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-06-16 20:46:52 -0700

    modify pivotfcn

--- dsa-javascript-udemy/6-sorting/intermidiate/pivotfcn.js
+++ dsa-javascript-udemy/6-sorting/intermidiate/pivotfcn.js
@@ -1,37 +1,23 @@
-// funcion acepta tres argumento, A, inicio y fin
-function pivot(arr, start=0, end= arr.length - 1) {
-    //funcion de intercambio
-    function swap(arr, i, j){
-        let temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
-
-    // elige al pivote para ser el inicio del array
-    let pivotElem = arr[start];
-    // Guarda el indice del pivote actual en una variable
+// funcion que acepte 3 argumentos: array, indice_inicio, indice_fin 
+function pivot(arr, start = 0, end = arr.length ) {
+    let pivot = arr[start];
     let pivotIndex = start;
-    // Loop el array de inicio a fin
-    for (let i = start + 1; i <= end; i++) {
-        // si el elemento actual es mayor al pivote
-        // incrementa el indice del pivote en 1
-        // despues, intercambia el elemento actual
-        // con el elemento en el indice del pivote
-        if (pivotElem > arr[i]) {
+    
+    for(let i = start + 1; i < end; i++){
+        if(pivot > arr[i]){
             pivotIndex++;
-            swap(arr, i , pivotIndex);
+            swap(arr, i, pivotIndex);
         }
     }
-    //intercambia el elemento inicial con el
-    //  elemento con el indice del pivote.
-    swap(arr, start, pivotIndex)
-    // regresa el inidice del pivote
+
+    swap(arr, start, pivotIndex);
     return pivotIndex;
 }
 
+function swap(arr, i , j){
+    [arr[i], arr[j]] = [arr[j], arr[i]]
+}
 
 let arr = [4, 1, 2, 8, 5, 7, 6, 3];
-
 console.log(pivot(arr));
-
-console.log(arr);
+console.log(arr)
