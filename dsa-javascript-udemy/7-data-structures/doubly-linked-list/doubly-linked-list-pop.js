7da535cc369d708a3295d771528ef4e0352248a1
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-06-20 21:58:53 -0700

    add pop method to doubly linked list

--- dsa-javascript-udemy/7-data-structures/doubly-linked-list/doubly-linked-list-pop.js
+++ dsa-javascript-udemy/7-data-structures/doubly-linked-list/doubly-linked-list-pop.js
@@ -40,22 +40,34 @@ class DoublyLinkedList {
     }
 
     pop(){
+        //verifica que haya elementos
         if(!this.head){
             return undefined;
         }
-        
+        // guarda el elemento a remover (tail)
         let popped = this.tail;
+        //si la longitu es 1 haz tail y head = null
         if(this.length === 1){
             this.head = null;
             this.tail = null;
         }
+        //si no, quita la conexion entre el ultimo nodo
+        // y el anteror, haz el nodo anterior la tail
         else {
             this.tail = popped.prev;
             popped.prev = null;
             this.tail.next = null;
         }
-
+        // decrementa la longitud
         this.length--;
+        //retorna el elemento eliminado
         return popped;
     }
 }
+let dll = new DoublyLinkedList();
+dll.push(1);
+dll.push(4);
+dll.push(8);
+dll.print();
+console.log(dll.pop());
+dll.print();
