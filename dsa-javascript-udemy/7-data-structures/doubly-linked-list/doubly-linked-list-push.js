b6aedc83f182be442cffbfad9a1ff974b0088810
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-06-20 21:34:09 -0700

    add push method to doubly linked list

--- dsa-javascript-udemy/7-data-structures/doubly-linked-list/doubly-linked-list-push.js
+++ dsa-javascript-udemy/7-data-structures/doubly-linked-list/doubly-linked-list-push.js
@@ -22,4 +22,26 @@ class DoublyLinkedList {
         }
         console.log(list);
     }
+
+    push(val){
+        let node = new Node(val);
+        if(!this.head){
+            this.head = node;
+            this.tail = node;
+        }
+        else {
+            this.tail.next = node;
+            node.prev = this.tail;
+            this.tail = node;
+        }
+
+        this.length++;
+        return this;
+    }
 }
+
+let dll = new DoublyLinkedList();
+dll.push(1);
+dll.push(4);
+dll.push(8);
+dll.print();
