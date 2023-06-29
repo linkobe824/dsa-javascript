206d058300cd3f7c5f939064450edda2050e532c
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-06-28 22:03:12 -0700

    add depth-first search in order to tree ds

--- dsa-javascript-udemy/7-data-structures/trees/tree-traversal/bfs.js
+++ dsa-javascript-udemy/7-data-structures/trees/tree-traversal/bfs.js
@@ -39,7 +39,12 @@ class BinarySearchTree {
     }
 
     //metodos de recorrido
-    // breadth-first search (iterativo)
+    // breadth-first search (iterativo) - Recorrido horizontal
+    // iniciando con la raiz del arbol en la cola
+    // se hace dequeue en la cola
+    // se agrega su valor (del nodo dequeued) en una variable para retornarla
+    // luego se verifica si tiene hijos y si los tiene se agregan a la cola
+    // entonces se repite el proceso hasta que ya no haya nodos en la cola.
     bfs(){
         //crea una queue y una variable para guardar el valor
         //de los nodos visitados
@@ -64,10 +69,18 @@ class BinarySearchTree {
     }
 
     //Depth First Search - PreOrder (recurisvo)
+    // se inicia con la raiz, y se van agregando los nodos del lado izquierdo
+    // antes que los del derecho
     dfs_preOrder(){
+        // crea una variable para guardar los nodos visitados
+        // y una variable current inicializada en root;
         let data = []
         let current = this.root;
 
+        // crea una helper function que tome como argumento un nodo
+        // el valor del nodo se añade a la variable para guardar
+        // si hay nodo a la izquierda utilizamos la helper function con ese nodo
+        // si hay nodo a la derecha utilizamos la gelper function con ese nodo
         const traverse = (node) => {
             data.push(node.val);
             if(node.left) traverse(node.left);
@@ -75,9 +88,52 @@ class BinarySearchTree {
         }
 
         traverse(current);
-
+        //retorna los datos
         return data;
 
+    }
+
+    //Depth Frist Search - PostOrder(recursivo)
+    // antes de agregar un nodo (visitarlo) se revisan los nodos izquierdo y derecho
+    dfs_postOrder(){
+        // crea una variable para almacenar los nodos visitados
+        let data = [];
+        // guarda la raiz del arbol en una variable llamada current
+        let current = this.root;
+        // crea una helper function que tome como argumento un nodo
+        const traverse = (node) => {
+            // si el nodo tiene propiedad izquierda, usa la helper function
+            // con la propiedad izquierda
+            if(node.left) traverse(node.left);
+            // si el nodo tiene propiedad derecha, usa la helper function
+            // con la propiedad derecha.
+            if(node.right) traverse(node.right);
+            // añade el valor del nodo a la variable
+            data.push(node.val);
+        }
+
+        // invoca a la helper function con current
+        traverse(current);
+        // retorna la variable con los nodos guardados.
+        return data;
+    }
+
+    //depth-first search - InOrder
+    //explora el lado izq, luego agrega el nodo, y explora el lado derecho (en orden)
+    dfs_inOrder(){
+        let data = [];
+        let current = this.root;
+
+        const traverse = (node) => {
+            if(node.left) traverse(node.left);
+            
+            data.push(node.val);
+
+            if(node.right) traverse(node.right);
+        }
+
+        traverse(current);
+        return data;
     }
 }
 
@@ -96,3 +152,7 @@ bt.insert(20);
 // console.log(bt.bfs()); //[10, 6, 15, 3 ,8 ,20]
 
 //console.log(bt.dfs_preOrder()) // [10 ,6, 3, 8, 15, 20]
+
+// console.log(bt.dfs_postOrder()); //[ 3, 8, 6, 20, 15, 10 ]
+
+// console.log(bt.dfs_inOrder()); //[ 3, 6, 8, 10, 15, 20 ]
