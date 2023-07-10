d9d3e06c855e449951d4ade702b6a46c608d51da
Manuel Beltran <136205896+linkobe824@users.noreply.github.com>
2023-07-10 15:58:35 -0700

    add removeEdge method to graph ds

--- dsa-javascript-udemy/7-data-structures/graph/graph.js
+++ dsa-javascript-udemy/7-data-structures/graph/graph.js
@@ -1,4 +1,4 @@
- class Graph {
+class Graph {
     constructor() {
         this.adjacencyList = {};
     }
@@ -6,10 +6,30 @@
     addVertex(vertex) {
         if(!this.adjacencyList[vertex])  this.adjacencyList[vertex] = [];
     }
-    
+
     addEdge(vertex1, vertex2){
         this.adjacencyList[vertex1].push(vertex2)
         this.adjacencyList[vertex2].push(vertex1)
     }
- }
 
+    // removeEdge(vertex1, vertex2){
+    //     this.adjacencyList[vertex1].splice(vertex2, 1)
+    //     this.adjacencyList[vertex2].splice(vertex1, 1)
+    // }
+
+    removeEdge(vertex1, vertex2){
+        this.adjacencyList[vertex1] = this.adjacencyList[vertex1]
+                                            .filter(v => v !== vertex2);
+        this.adjacencyList[vertex2] = this.adjacencyList[vertex2]
+                                            .filter(v => v !== vertex1);
+    }
+}
+
+let g = new Graph()
+g.addVertex('tokyo')
+g.addVertex('dallas')
+g.addVertex('culiacan')
+
+g.addEdge('tokyo','culiacan')
+g.addEdge('tokyo','dallas')
+g.addEdge('dallas','culiacan')
